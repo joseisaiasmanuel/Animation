7998461d1e79c36a1d39c2c8e8850841fdde61a0
jose1704 <joseisaiasmanuel@gmail.com>
2021-03-17 23:01:00 +0100

    animation

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class LogoApp extends StatefulWidget {
 class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   AnimationController controller;
   Animation<double> animation;
+  Animation <double> animation2;
 
   @override
   void initState() {
@@ -39,7 +40,18 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
       }
 
     });
+    animation2 = Tween<double>(begin: 0, end: 150).animate(controller);
+    animation2.addStatusListener((status) {
+      if(status ==AnimationStatus.completed){
+        controller.reverse();
+      }else if (status == AnimationStatus.dismissed){
+        controller.forward();
+
+      }
+
+    });
     controller.forward();
+
   }
 
   @override
@@ -50,11 +62,22 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedLogo(animation);
+    return Column(
+      children:<Widget> [
+    GrowTransition(
+    child: LogoWidget(),
+    animation:animation
+    ) ,
+        GrowTransition(
+            child: LogoWidget(),
+            animation:animation2
+        )
+      ],
+    );
   }
 }
 
-class AnimatedLogo extends AnimatedWidget{
+/*class AnimatedLogo extends AnimatedWidget{
   AnimatedLogo(Animation<double> animation): super(listenable: animation);
   @override
   Widget build(BuildContext context) {
@@ -68,4 +91,37 @@ class AnimatedLogo extends AnimatedWidget{
      );
   }
 
+} */
+
+class LogoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlutterLogo(),
+    );
+  }
 }
+
+class GrowTransition extends StatelessWidget {
+  final Widget child;
+  final Animation<double> animation;
+  GrowTransition({this.child, this.animation});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context,child){
+          return Container(
+            height: animation.value,
+            width: animation.value,
+            child:child,
+          );
+        },
+        child: child,
+      ),
+    );
+  }
+}
+
+
